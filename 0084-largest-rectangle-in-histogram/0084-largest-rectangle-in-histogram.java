0f242f0efed43c8f261b26e6cebb776e7057abec
Tanvir Ahmed <tanvir.mclv@gmail.com>
2024-05-11 03:44:06 -0700

    Time: 69 ms (57.01%), Space: 56 MB (95.92%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,44 +1,16 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
         int maxArea = 0;
-
         Stack<int[]> stk = new Stack<>();
-        for (int i = 0; i < heights.length; i++) {
-            if (stk.isEmpty()) {
-                stk.push(new int[] {heights[i], i});
-            }
-            else {
-                int[] valueIndexPair = stk.peek();
-                if (valueIndexPair[0] <= heights[i]) {
-                    stk.push(new int[] {heights[i], i});
-                }
-                else {
-                    while (!stk.isEmpty() && valueIndexPair[0] > heights[i]) {
-                        //can not extend the rectangle anymore
-                        valueIndexPair = stk.pop();
-                        int left = -1;
-                        if (!stk.isEmpty()) {
-                            left = stk.peek()[1];
-                        }
-                        int area = (i - left - 1) * valueIndexPair[0];
-                        maxArea = Math.max(maxArea, area);
-                        if (!stk.isEmpty()) {
-                            valueIndexPair = stk.peek();
-                        }
-                    }
-                    stk.push(new int[] {heights[i], i});
-                }
-            }
-        }
 
-        while (!stk.isEmpty()) {
-            int[] valueIndexPair = stk.pop();
-            int left = -1;
-            if (!stk.isEmpty()) {
-                left = stk.peek()[1];
+        for (int i = 0; i <= heights.length; i++) {
+            int currHeight = (i == heights.length) ? 0 : heights[i];
+            while (!stk.isEmpty() && stk.peek()[0] > currHeight) {
+                int[] popped = stk.pop();
+                int width = stk.isEmpty() ? i : i - stk.peek()[1] - 1;
+                maxArea = Math.max(maxArea, popped[0] * width);
             }
-            int area = (heights.length - left - 1) * valueIndexPair[0];
-            maxArea = Math.max(maxArea, area);
+            stk.push(new int[] {currHeight, i});
         }
 
         return maxArea;
