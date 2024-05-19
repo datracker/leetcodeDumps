e098a13beec1435be02b53ea1269c10a3ef51f87
Tanvir Ahmed <tanvir.mclv@gmail.com>
2024-05-19 02:31:19 -0700

    Time: 0 ms (100%), Space: 41.5 MB (23.87%) - LeetHub

--- 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.java
+++ 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.java
@@ -1,40 +1,22 @@
 class Solution {
     public int numSteps(String s) {
-        int steps = 0;
-        while (s.length() > 1) {
-            if (isEven(s)) {
-                s = rightShift(s);
+       int steps = 0;
+        int n = s.length();
+        int carry = 0;
+
+        // Iterate from the least significant bit to the most significant bit
+        for (int i = n - 1; i > 0; i--) {
+            if ((s.charAt(i) - '0' + carry) % 2 == 0) {
+                // If even, just shift right
+                steps++;
+            } else {
+                // If odd, add one (carry over)
+                carry = 1;
+                steps += 2; // one for addition and one for shifting
             }
-            else {
-                s = addOne(s);
-            }
-            steps++;
         }
 
-        return steps;
-    }
-
-    boolean isEven(String s) {
-        return s.charAt(s.length()-1) == '0';
-    }
-
-    String rightShift(String s) {
-        return s.substring(0, s.length()-1);
-    }
-
-    String addOne(String s) {
-        StringBuilder sb = new StringBuilder();
-
-        int carry = 1;
-        for (int i = s.length()-1; i >= 0; i--) {
-            int a = s.charAt(i) - '0';
-            int sum = a ^ carry;
-            carry = a & carry;
-
-            sb.append(sum);
-        }
-        if (carry == 1) sb.append(carry);
-
-        return sb.reverse().toString();
+        // Add final step if there is a carry left
+        return steps + carry;
     }
 }
