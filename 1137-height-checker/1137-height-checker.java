cc0cdf24ae69ea0e404fb43c22a1c88deb46a6ce
Tanvir Ahmed <tanvir.mclv@gmail.com>
2024-06-09 19:46:45 -0700

    Time: 0 ms (100%), Space: 41.3 MB (51.37%) - LeetHub

--- 1137-height-checker/1137-height-checker.java
+++ 1137-height-checker/1137-height-checker.java
@@ -1,13 +1,20 @@
 class Solution {
     public int heightChecker(int[] heights) {
-        int[] current = Arrays.copyOf(heights, heights.length);
-        Arrays.sort(heights);
+        int[] freq = new int[101];
+        for (int i = 0; i < heights.length; i++) {
+            freq[heights[i]]++;
+        }
 
         int count = 0;
-        for (int i = 0; i < heights.length; i++) {
-            if (current[i] != heights[i]) {
-                count++;
-            }
+        int start = 0;
+        for (int i = 1; i < 101; i++) { 
+            int countShouldBe = freq[i];
+            while (countShouldBe > 0) {
+                if (heights[start++] != i) {
+                    count++;
+                }
+                countShouldBe--;
+            }                    
         }
 
         return count;
