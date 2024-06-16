b69ef20be2516efe210a0ffc16fce35eb70a583f
Tanvir Ahmed <tanvir.mclv@gmail.com>
2024-06-16 16:33:48 -0700

    Time: 29 ms (11.36%), Space: 58.2 MB (39.77%) - LeetHub

---
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean checkSubarraySum(int[] nums, int k) {
+        Map<Integer, Integer> prefixRemainderIdx = new HashMap<>();
+        prefixRemainderIdx.put(0, -1);
+        int sum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            int rem = sum % k;
+
+            if (prefixRemainderIdx.containsKey(rem) && (i-prefixRemainderIdx.get(rem)) > 1) {
+                return true;
+            }
+            prefixRemainderIdx.put(rem, 
+                Math.min(i, prefixRemainderIdx.getOrDefault(rem, Integer.MAX_VALUE)));
+        }
+
+        return false;
+    }
+}
