233ba1332c2f3893f232b485d38d125fd79f70a3
Tanvir Ahmed <tanvir.mclv@gmail.com>
2024-06-22 15:54:43 -0700

    Time: 5 ms (21.37%), Space: 47.6 MB (19.14%) - LeetHub

---
+++ 1138-grumpy-bookstore-owner/1138-grumpy-bookstore-owner.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int maxSatisfied(int[] customers, int[] grumpy, int minutes) {
+        int left = 0;
+        int total_satisfied = 0;
+        int window = 0;
+        int max_window = 0;
+        for (int right = 0; right < customers.length; right++) {
+            if (grumpy[right] == 0) {
+                total_satisfied += customers[right];
+            }
+            else {
+                window += customers[right];
+            }
+            if (right - left + 1 > minutes) {
+                if (grumpy[left] == 1) {
+                    window -= customers[left];
+                }
+                left++;
+            }
+            max_window = Math.max(max_window, window);
+        }
+
+        return total_satisfied + max_window;
+    }
+}
