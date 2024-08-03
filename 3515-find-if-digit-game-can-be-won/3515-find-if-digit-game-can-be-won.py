fe21daadf772b443c7ebdc59a70f1b9aef246f09
Tanvir Ahmed <tanvir.mclv@gmail.com>
2024-08-03 07:06:58 -0700

    Time: 47 ms (95.55%), Space: 16.6 MB (25.42%) - LeetHub

---
+++ 3515-find-if-digit-game-can-be-won/3515-find-if-digit-game-can-be-won.py
@@ -0,0 +1,12 @@
+class Solution:
+    def canAliceWin(self, nums: List[int]) -> bool:
+        single_sum = 0
+        double_sum = 0
+        for num in nums:
+            if num < 10:
+                single_sum += num
+            else:
+                double_sum += num
+        
+        return not single_sum == double_sum
+        
