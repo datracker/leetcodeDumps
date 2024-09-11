06e38f56d1e91078ade95fcf79f57eb1e6ab2511
Tanvir Ahmed <tanvir.mclv@gmail.com>
2024-09-10 20:20:24 -0700

    Time: 0 ms (100%), Space: 39.8 MB (99.65%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.java
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.java
@@ -1,12 +1,6 @@
 class Solution {
     public int minBitFlips(int start, int goal) {
         int diff = start ^ goal;
-        int count = 0;
-        while (diff > 0) {
-            count += (diff & 1);
-            diff = diff >> 1;
-        }
-
-        return count;
+        return Integer.bitCount(diff);        
     }
 }
