6f2b57300aa96048df0dde6048e90ccd838f679b
Tanvir Ahmed <tanvir.mclv@gmail.com>
2024-09-10 21:41:17 -0700

    Time: 6 ms (45.72%), Space: 44.7 MB (54.43%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,38 +1,39 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        List<Integer>[] adjList = new ArrayList[numCourses];
         int[] indegree = new int[numCourses];
+        List<List<Integer>> adj = new ArrayList<>(numCourses);
 
         for (int i = 0; i < numCourses; i++) {
-            adjList[i] = new ArrayList<>();
+            adj.add(new ArrayList<>());
         }
 
-        for (int[] prereq : prerequisites) {
-            int u = prereq[1];
-            int v = prereq[0];
-            adjList[u].add(v);
-            indegree[v]++;
+        for (int[] prerequisite : prerequisites) {
+            adj.get(prerequisite[1]).add(prerequisite[0]);
+            indegree[prerequisite[0]]++;
         }
 
-        Queue<Integer> q = new LinkedList<>();
+        Queue<Integer> queue = new LinkedList<>();
+        // Push all the nodes with indegree zero in the queue.
         for (int i = 0; i < numCourses; i++) {
             if (indegree[i] == 0) {
-                q.offer(i);
+                queue.offer(i);
             }
         }
 
-        int nodeVisited = 0; 
-        while (!q.isEmpty()) {
-            int node = q.poll();
-            nodeVisited++;
-            for (int neighbor : adjList[node]) {
+        int nodesVisited = 0;
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            nodesVisited++;
+
+            for (int neighbor : adj.get(node)) {
+                // Delete the edge "node -> neighbor".
                 indegree[neighbor]--;
                 if (indegree[neighbor] == 0) {
-                    q.offer(neighbor);
+                    queue.offer(neighbor);
                 }
             }
         }
 
-        return nodeVisited == numCourses;
+        return nodesVisited == numCourses;
     }
 }
