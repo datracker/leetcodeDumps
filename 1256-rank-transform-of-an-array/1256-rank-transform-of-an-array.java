dddc5c2b99702d388d361391244abfae5a42c2ad
Tanvir Ahmed <tanvir.mclv@gmail.com>
2024-10-07 03:17:18 -0700

    Time: 28 ms (64.29%), Space: 62.2 MB (39.01%) - LeetHub

--- 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.java
+++ 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.java
@@ -1,27 +1,27 @@
 class Solution {
     public int[] arrayRankTransform(int[] arr) {
-        Queue<Integer> pq = new PriorityQueue<>();
-        Map<Integer, List<Integer>> idMap = new HashMap<>();
+        if (arr.length == 0) return new int[0];
         
-        for (int i = 0; i < arr.length; i++) {
-            if (!idMap.containsKey(arr[i])) {
-                idMap.put(arr[i], new ArrayList<>());
-                pq.offer(arr[i]);
-            }
-            idMap.get(arr[i]).add(i);
-        }
-
-        int[] res = new int[arr.length];
+        // Create a sorted version of the unique values
+        int[] sortedArr = arr.clone();
+        Arrays.sort(sortedArr);
+        
+        // Create a map to store the rank of each unique value
+        Map<Integer, Integer> rankMap = new HashMap<>();
         int rank = 1;
-        while (!pq.isEmpty()) {
-            int num = pq.poll();
-            List<Integer> indices = idMap.get(num);
-            for (int index : indices) {                
-                res[index] = rank;                
+        for (int num : sortedArr) {
+            if (!rankMap.containsKey(num)) {
+                rankMap.put(num, rank++);
             }
-            rank++;
         }
-
+        
+        // Build the result array using the rank map
+        int[] res = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            res[i] = rankMap.get(arr[i]);
+        }
+        
         return res;
     }
+
 }
